e7a503af24131ec8641af6f1a848d9183622d564
dhruv behl <dhruvbhl@gmail.com>
2017-02-24 12:01:03 +0530

    removing debug statements

--- PMI/Melodica/playchords.py
+++ PMI/Melodica/playchords.py
@@ -4,9 +4,5 @@ import progressions as pr
 def playChords(chord, ctype):
 	prog = []
 	prog = pr.getChordSeq(chord, ctype)
-	print prog
-	print type(prog)
 	c = Chord(prog)
 	c.show('midi')
-
-playChords('A','maj')
