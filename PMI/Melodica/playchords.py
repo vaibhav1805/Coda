55a9196a9778aac1fdd373276d9d2f7d994da5d6
dhruv behl <dhruvbhl@gmail.com>
2017-02-24 12:29:03 +0530

    adding instrument support

--- PMI/Melodica/playchords.py
+++ PMI/Melodica/playchords.py
@@ -1,8 +1,18 @@
+from music21 import stream, instrument
 from music21.chord import * 
 import progressions as pr
+
+def instr(chord):
+	instrumentPart = stream.Part()
+	instrumentPart.insert(0, instrument.Guitar())
+	instrumentMeasure = stream.Measure()
+	instrumentMeasure.append(chord)
+	instrumentPart.append(instrumentMeasure)
+	instrumentPart.show('midi')
+
 
 def playChords(chord, ctype):
 	prog = []
 	prog = pr.getChordSeq(chord, ctype)
 	c = Chord(prog)
-	c.show('midi')
+	instr(c)
