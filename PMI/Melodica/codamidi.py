4c10045161ecd6b11a644b4ce3a7a855a505a5e0
vaibhavk <vaibhav9k2@gmail.com>
2017-03-06 00:42:01 +0530

    MIDI chords

--- PMI/Melodica/codamidi.py
+++ PMI/Melodica/codamidi.py
@@ -3,13 +3,17 @@ import notemap as nm
 import progressions as pg
 import math
 
+def initMIDI(track=0, channel=0, time=0, duration=0, tempo=60, volume=100):
+	codaMIDI = MIDIFile(1)
+	codaMIDI.addTempo(track, time, tempo)
+	return codaMIDI
+
 def midiToPitch(p):
 	pitch = pow(2.0, ((p-69))/12.0)*440
 	return round(pitch, 1)
 
 def noteToMidi(p, octave):
 	p = nm.getNoteFreq(p, octave)
-	print p
 	midi = (math.log(float(p/440), 2)*12) + 69
 	return int(round(midi))
 
@@ -38,6 +42,13 @@ def playScale(root, octave, mood):
 		MyMIDI.addNote(track, channel, midi+i, t, duration, volume)
 		t = t +1
 
-playScale("C", 6, "maj")
-with open("major-scale.mid", "wb") as output_file:
-    MyMIDI.writeFile(output_file)
+def playChord(root, octave, mood):
+	midi = noteToMidi(root, octave)
+	t = time
+	for i in pg.types[mood]:
+		MyMIDI.addNote(track, channel, midi+i, t, duration, volume)
+	t=t+1
+
+#playChord("A", 2, "min")
+# with open("major-scale.mid", "wb") as output_file:
+#     MyMIDI.writeFile(output_file)
